ff1cafdd18ba5f3eab670a1fa93a2acddbe866eb
Fabricio <fabricio_lopes@outlook.com.br>
2023-07-11 21:25:05 -0300

    feat: server express running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("O Grêmio vai sair campeão");
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get('/teste', (request, response) => {
+    response.send(`Atenção terráqueos, o Grêmio vai sair campeão! E quem é de é, é! Quem não é de é deixou de é!`)
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
